2c98f6d968eb7e2a87bb9ba82f71bba00995e7eb
Rija Menage <rija+git@cinecinetique.com>
2019-10-10 20:06:38 +0200

    Add a token service class to generate JWT token for API calls (wip)
    It will be used (and is therefore a pre-requisite) by the FiledropService
    that powers the UX for assigning file upload dropbox to a dataset.
    
    It is part of the GigaDB's Yii1-based codebase and test harness but uses Yii2 component system
    (inheritance and configuration).
    
    It's still work in progress. UserDAO is not implemented yet.

--- protected/config/yii2/web.php
+++ protected/config/yii2/web.php
@@ -3,5 +3,11 @@
 return [
 	'id' => 'GigaDB Web Site',
 	'basePath' => '/var/www',
+	'components' => [
+        'jwt' => [
+            'class' => 'sizeg\jwt\Jwt',
+            'key'   => 'dummy--qwertyuiop' #$params['jwt_key'],
+        ],
+    ],
 ];
 ?>
