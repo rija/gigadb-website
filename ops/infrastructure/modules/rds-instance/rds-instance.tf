50dd9a281845cdf22c7b05ef1d8f5aa80642cd26
Peter Li <peter@gigasciencejournal.com>
2021-08-19 16:28:58 +0800

    Remove modules
    Hardcoded name of the created RDS instance has been changed too.

--- ops/infrastructure/modules/rds-instance/rds-instance.tf
+++ ops/infrastructure/modules/rds-instance/rds-instance.tf
@@ -5,7 +5,7 @@ provider "aws" {
 }
 
 locals {
-  name   = "complete-postgresql"
+  name   = "rds-ape1-test-postgres-gigadb"
   region = "ap-east-1"
   tags = {
     Owner       = "user"
@@ -60,71 +60,6 @@ module "security_group" {
 # RDS Module
 ################################################################################
 
-module "db" {
-  source  = "terraform-aws-modules/rds/aws"
-
-  identifier = local.name
-
-  # All available versions: https://docs.aws.amazon.com/AmazonRDS/latest/UserGuide/CHAP_PostgreSQL.html#PostgreSQL.Concepts
-  engine               = "postgres"
-  engine_version       = "11.10"
-  family               = "postgres11" # DB parameter group
-  major_engine_version = "11"         # DB option group
-  instance_class       = "db.t3.micro"
-
-  allocated_storage     = 20
-  max_allocated_storage = 100
-  storage_encrypted     = false
-
-  # NOTE: Do NOT use 'user' as the value for 'username' as it throws:
-  # "Error creating DB Instance: InvalidParameterValue: MasterUsername
-  # user cannot be used as it is a reserved word used by the engine"
-  name     = "completePostgresql"
-  username = "complete_postgresql"
-  password = "YourPwdShouldBeLongAndSecure!"
-  port     = 5432
-
-  multi_az               = true
-  subnet_ids             = module.vpc.database_subnets
-  vpc_security_group_ids = [module.security_group.security_group_id]
-
-  maintenance_window              = "Mon:00:00-Mon:03:00"
-  backup_window                   = "03:00-06:00"
-  enabled_cloudwatch_logs_exports = ["postgresql", "upgrade"]
-
-  backup_retention_period = 0
-  skip_final_snapshot     = true
-  deletion_protection     = false
-
-  performance_insights_enabled          = true
-  performance_insights_retention_period = 7
-  create_monitoring_role                = true
-  monitoring_interval                   = 60
-
-  parameters = [
-    {
-      name  = "autovacuum"
-      value = 1
-    },
-    {
-      name  = "client_encoding"
-      value = "utf8"
-    }
-  ]
-
-  tags = local.tags
-  db_option_group_tags = {
-    "Sensitive" = "low"
-  }
-  db_parameter_group_tags = {
-    "Sensitive" = "low"
-  }
-  db_subnet_group_tags = {
-    "Sensitive" = "high"
-  }
-}
-
-
 module "db_default" {
   source = "terraform-aws-modules/rds/aws"
 
@@ -160,15 +95,4 @@ module "db_default" {
   backup_retention_period = 0
 
   tags = local.tags
-}
-
-module "db_disabled" {
-  source = "terraform-aws-modules/rds/aws"
-
-  identifier = "${local.name}-disabled"
-
-  create_db_instance        = false
-  create_db_subnet_group    = false
-  create_db_parameter_group = false
-  create_db_option_group    = false
 }
