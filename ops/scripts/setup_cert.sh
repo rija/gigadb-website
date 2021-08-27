cee2f84da0265df4a04a42cecc95974d4cbcbbde
Rija Ménagé <github@rijam.sent.as>
2021-08-27 20:33:55 +0200

    Add dry run code for backup when cert local exists

--- ops/scripts/setup_cert.sh
+++ ops/scripts/setup_cert.sh
@@ -52,8 +52,12 @@ echo "chain_pem_remote_exists: $chain_pem_remote_exists"
 if [[ $cert_files_local_exists == 'true' ]];then
 	echo "Renewing the certificate for $REMOTE_HOSTNAME"
 	$DOCKER_COMPOSE run --rm certbot renew
-#	echo "Backup the fullchain cert to gitlab variable"
-#	if [ $fullchain_pem_remote_exists == "true" ];then
+	echo "Backup the fullchain cert to gitlab variable"
+	if [ $fullchain_pem_remote_exists == "true" ];then
+	  echo "/usr/bin/curl --show-error --silent --request PUT --url '$CI_PROJECT_URL/variables/tls_fullchain_pem?filter%5benvironment_scope%5d=$GIGADB_ENV' --header 'PRIVATE-TOKEN: $GITLAB_PRIVATE_TOKEN' --form 'environment_scope=$GIGADB_ENV' --form 'value=@$FULLCHAIN_PEM'"
+	else
+	  echo "/usr/bin/curl -L --show-error --silent --request POST --url '$CI_PROJECT_URL/variables' --header 'PRIVATE-TOKEN: $GITLAB_PRIVATE_TOKEN' --form 'environment_scope=$GIGADB_ENV' --form 'key=tls_fullchain_pem' --form 'value=@$FULLCHAIN_PEM'"
+	fi
 #    $DOCKER_COMPOSE run --rm config bash -c "/usr/bin/curl --show-error --silent --request PUT --url '$CI_PROJECT_URL/variables/tls_fullchain_pem?filter%5benvironment_scope%5d=$GIGADB_ENV' --header 'PRIVATE-TOKEN: $GITLAB_PRIVATE_TOKEN' --form 'environment_scope=$GIGADB_ENV' --form 'value=@$FULLCHAIN_PEM'"
 #  else
 #    $DOCKER_COMPOSE run --rm config bash -c "/usr/bin/curl -L --show-error --silent --request POST --url '$CI_PROJECT_URL/variables' --header 'PRIVATE-TOKEN: $GITLAB_PRIVATE_TOKEN' --form 'environment_scope=$GIGADB_ENV' --form 'key=tls_fullchain_pem' --form 'value=@$FULLCHAIN_PEM'"
