11caeda2fa9db90e3fbffa875853bfd327996a21
Ken <kencho.gigascience@gmail.com>
2021-09-02 14:21:49 +0800

    Include steps to load upgraded dump into postgresql server

--- sql/convert_production_db_to_latest_ver.sh
+++ sql/convert_production_db_to_latest_ver.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
+# Load environment variables
 source ./.env
 source ./.secrets
 
@@ -12,6 +13,9 @@ else
 	DOCKER_COMPOSE="docker-compose"
 fi
 
+echo "Spin up the database container"
+docker-compose up -d --build database
+
 echo "Go into docker container and store the postgreSQL version"
 version=$($DOCKER_COMPOSE run --rm test bash -c "psql --version | cut -d' ' -f 3 | tr -d '\n'")
 
@@ -23,18 +27,35 @@ cd ../../../../
 echo "Create production database for the version upgrade"
 $DOCKER_COMPOSE run --rm test bash -c "psql -h database -U gigadb -c 'create database gigadbv3_production'"
 
-echo "Load the production database in PostgreSQL"
+echo "Load the production database into PostgreSQL server"
 $DOCKER_COMPOSE run --rm test bash -c "PGPASSWORD=$GIGADB_PASSWORD pg_restore -v -U gigadb -h database -p 5432 -d gigadbv3_production /var/www/gigadb/app/tools/files-url-updater/sql/gigadbv3_${latest}.backup"
 
-echo "Dump production database"
+echo "Create folder for database dump if not existed"
+if [[ ! -d sql/psql-v96 ]];then
+  mkdir sql/psql-v96
+fi
+
+echo "Dump the production database"
 #$DOCKER_COMPOSE run --rm test bash -c "PGPASSWORD=$GIGADB_PASSWORD pg_dump -v -U gigadb -h database -p 5432 -d gigadbv3_production -f /var/www/sql/psql-v96/gigadbv3_${latest}_${version}.sql"
 $DOCKER_COMPOSE run --rm test bash -c "PGPASSWORD=$GIGADB_PASSWORD pg_dump -v -U gigadb -h database -p 5432 -Fc -d gigadbv3_production -f /var/www/sql/psql-v96/gigadbv3_${latest}_${version}.pgdmp"
-
-echo "Drop production database after conversion"
-$DOCKER_COMPOSE run --rm test bash -c "psql -h database -U gigadb -c 'drop database gigadbv3_production'"
 
 if [[ -f sql/psql-v96/gigadbv3_"$latest"_"$version".pgdmp ]];then
   echo "Finished convert production database to postgreSQL version" "$version"
 else
   echo "No upgraded database dump found, conversion fail!"
 fi
+
+echo "Load the upgraded dump into PostgreSQL server"
+$DOCKER_COMPOSE run --rm test bash -c "PGPASSWORD=$GIGADB_PASSWORD pg_restore -v -U gigadb -h database -p 5432 --clean -d gigadbv3_production /var/www/sql/psql-v96/gigadbv3_${latest}_${version}.pgdmp"
+
+if [[ $? -eq 0 ]];then
+  echo "The upgraded dump could be restored!"
+else
+  echo "The upgraded dump could not be restored, please check!"
+fi
+
+echo "Drop production database after upgraded dump has been restored"
+$DOCKER_COMPOSE run --rm test bash -c "psql -h database -U gigadb -c 'drop database gigadbv3_production'"
+
+echo "Stop database container"
+docker stop deployment_database_1
