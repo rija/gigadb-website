587e1359344c97c54673e2a36b21ff3fa01a1729
Peter Li <peter@gigasciencejournal.com>
2021-10-18 16:13:45 +0800

    Add AWS username to name tag of bastion server
    AWS username has been added to the end of the name tag of bastion
    server. This allows bastion servers to distinguished from one
    another and I'm sure EC2 servers are not allowed to have a name tag
    that is the same as the name tag of another EC2 server.

--- ops/infrastructure/modules/bastion-aws-instance/bastion-aws-instance.tf
+++ ops/infrastructure/modules/bastion-aws-instance/bastion-aws-instance.tf
@@ -34,7 +34,7 @@ resource "aws_instance" "bastion" {
   subnet_id = var.public_subnet_id
 
   tags = {
-    Name = "bastion_server_${var.deployment_target}",
+    Name = "bastion_server_${var.deployment_target}_${var.owner}",
     System = "t3_micro-centos8",
   }
 
