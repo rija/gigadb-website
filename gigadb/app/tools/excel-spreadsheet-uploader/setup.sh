63efdf9077de88e0405c93489b776d29d96611ec
Rija Ménagé <rm.dev.git.commits@eml.cc>
2023-07-18 15:54:50 +0200

    temp: make use of the postgresql jar branch for ExcelToGigaDB

--- gigadb/app/tools/excel-spreadsheet-uploader/setup.sh
+++ gigadb/app/tools/excel-spreadsheet-uploader/setup.sh
@@ -9,9 +9,11 @@ PATH=/usr/local/bin:$PATH
 export PATH
 
 # Download java source files
-docker-compose run --rm uploader curl -L -O https://github.com/gigascience/ExceltoGigaDB/archive/develop.zip
+# docker-compose run --rm uploader curl -L -O https://github.com/gigascience/ExceltoGigaDB/archive/develop.zip
+docker-compose run --rm uploader curl -L -O https://github.com/kencho51/ExceltoGigaDB/archive/refs/heads/update-jdbc-driver.zip
 
 # Unpack source files in place
-docker-compose run --rm uploader bsdtar -k --strip-components=1 -xvf develop.zip
+# docker-compose run --rm uploader bsdtar -k --strip-components=1 -xvf develop.zip
+docker-compose run --rm uploader bsdtar -k --strip-components=1 -xvf update-jdbc-driver.zip
 
 
