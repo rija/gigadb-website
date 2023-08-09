c0d3348f76118f2786e1760d3d3e1328312468ff
Rija Ménagé <rm.dev.git.commits@eml.cc>
2023-08-09 11:57:17 +0200

    test: use java code from feature branch

--- gigadb/app/tools/excel-spreadsheet-uploader/setup.sh
+++ gigadb/app/tools/excel-spreadsheet-uploader/setup.sh
@@ -9,9 +9,10 @@ PATH=/usr/local/bin:$PATH
 export PATH
 
 # Download java source files
-docker-compose run --rm uploader curl -L -O https://github.com/gigascience/ExceltoGigaDB/archive/develop.zip
+# docker-compose run --rm uploader curl -L -O https://github.com/gigascience/ExceltoGigaDB/archive/develop.zip
+docker-compose run --rm uploader curl -L -O https://github.com/kencho51/ExceltoGigaDB/archive/refs/heads/remove-adding-non-exist-sample-attribute-feature.zip
 
 # Unpack source files in place
-docker-compose run --rm uploader bsdtar -k --strip-components=1 -xvf develop.zip
+docker-compose run --rm uploader bsdtar -k --strip-components=1 -xvf remove-adding-non-exist-sample-attribute-feature.zip
 
 
